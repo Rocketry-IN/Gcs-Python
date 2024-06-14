9094ee81f22a2adf39e9f8880a2bae6ab1a8e4ee
Lalit <glalitkartikeyan@gmail.com>
2024-06-14 20:27:20 +0530

    buf fix

--- test-stand/serial_man.py
+++ test-stand/serial_man.py
@@ -11,7 +11,3 @@ def read_serial():
     incoming = ser.read().decode("utf-8")
     packet = incoming.split(",")
     return packet
-
-def send_serial(command):
-    outgoing = command.encode("utf-8")
-    ser.write(outgoing)
